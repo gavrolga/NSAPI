80e266fafbec380a5bc1318bdd41e26b0c360e73
gavrolga <goa883@gmail.com>
2026-05-24 22:05:55 +0300

    фикс notification status

--- app/Models/NotificationRecipient.php
+++ app/Models/NotificationRecipient.php
@@ -5,20 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Support\HigherOrderCollectionProxy;
 
 class NotificationRecipient extends Model
 {
     use HasFactory;
 
-    /**
-     * @var HigherOrderCollectionProxy|mixed
-     */
-    public mixed $notification;
-    /**
-     * @var HigherOrderCollectionProxy|mixed
-     */
-    public mixed $subscriber;
     protected $fillable = [
         'notification_id',
         'subscriber_id',
